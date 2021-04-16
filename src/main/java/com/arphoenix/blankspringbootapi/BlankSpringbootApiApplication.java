edec0b1575289057578d2dc54ebdf7307b23d4c6
rsdelia <rsdelia@RSDELIA6640>
2021-04-16 00:16:02 -0300

    subida inicial 15-04-2021

---
+++ src/main/java/com/arphoenix/blankspringbootapi/BlankSpringbootApiApplication.java
@@ -0,0 +1,13 @@
+package com.arphoenix.blankspringbootapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlankSpringbootApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlankSpringbootApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arphoenix/blankspringbootapi/BlankSpringbootApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arphoenix.blankspringbootapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlankSpringbootApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
